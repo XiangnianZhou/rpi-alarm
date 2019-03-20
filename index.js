a10e086cf115959b25e038511f4dd3fca3a82e81
raspberry pi <xn.moss@gmail.com>
2019-03-20 23:16:57 +0800

    test beep function

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const Gpio = require('onoff').Gpio;
-const beep = new Gpio(22, 'out');
-const lightInput = new Gpio(27, 'in');
-
-const beepOn  = () => {
-    beep.writeSync(1);
-    setTimeout(() => {
-        beep.writeSync(0);
-    }, 350);
-}
-
-console.log(lightInput.readSync())
+const beep = require('./beep');
+beep(0.1)

67bcf56ce80e5539e98bbf1114c6600570bc2640
XiangnianZhou <47637763+XiangnianZhou@users.noreply.github.com>
2019-03-30 23:18:02 +0800

    koa

---
+++ server.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+app.use(router.routes());
+
+app.get('/', async (ctx, next) => {
+    console.log(ctx.method);
+    console.log(ctx.query);
+    await next();
+});
+
+app.listen(3000);

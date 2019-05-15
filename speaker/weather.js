978958a11bfc5d148bd6d8cc3f7614facf8eb78a
XiangnianZhou <xn.moss@gmail.com>
2019-05-15 22:04:54 +0800

    新增puppeteer

--- speaker/weather.js
+++ speaker/weather.js
@@ -1,39 +1,8 @@
-const exec = require('child_process').exec;
-const request = require('request');
+const puppeteer = require('puppeteer');
 
-const xmlyAPI = 'https://www.ximalaya.com/revision/play/album?albumId=22689810&pageNum=1&sort=-1&pageSize=1';
 
-const playAudio = function (url) {
-  return new Promise(resolve => {
-    exec(`mplayer ${url}`, '', () => {
-      resolve();
-    });
-  });
-}
+(async () => {
+  const browser = await puppeteer.launch();
+  
 
-const getUrl = function getXmlyMediaUrl() {
-  return new Promise((resolve, rejects) => {
-    request.get(xmlyAPI, (error, response, body) => {
-      console.log(body)
-      try {
-        const res = JSON.parse(body);
-        if (!error && body && res.data && res.data.tracksAudioPlay) {
-          const url = res.data.tracksAudioPlay[0].src;
-          resolve(url);
-        } else {
-          rejects('喜马拉雅数据错误');
-        }
-      } catch(e) {
-        rejects(e);
-      }
-    });
-  });
-}
-
-module.exports = function  wether () {
-  getUrl().then(url => {
-    playAudio(url);
-  }).catch(e => {
-    // wether();
-  }) ;
-}
+})();

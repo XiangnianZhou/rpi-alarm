af9655af09366fac1e669ff51239fc22817688fd
XiangnianZhou <xn.moss@gmail.com>
2019-05-18 20:48:11 +0800

    使用新的喜马拉雅

--- speaker/weather.js
+++ speaker/weather.js
@@ -1,54 +1,38 @@
-const puppeteer = require('puppeteer');
 const exec = require('child_process').exec;
+const request = require('request');
 
-const xmlyPage = 'https://www.ximalaya.com/zhubo/7396493/';
+const xmlyAPI = 'http://m.ximalaya.com/mobile/v1/track/share/content?trackId=184619977&tpName=weixin&device=h5';
 
-const playAudio = (url) => {
+const playAudio = function (url) {
   return new Promise(resolve => {
-    exec(`mplayer ${url}`, '', () => [
-      resolve()
-    ]);
-  });
-}
-
-const getWeather = async () => {
-
-  const sleep = (time) => new Promise(resolve => {
-    setTimeout(resolve, time)
-  });
-
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(xmlyPage);
-  await page.waitForSelector('.black-tape._LmmS');
-  await page.click('.black-tape._LmmS');
-  
-  const getWeatherInfo = async() => {
-    await sleep(3000);
-    const list = page.evaluate(() => {
-      let playListStr = localStorage.getItem('player_playlist') || '';
-      let playList;
-      try {
-        playList = JSON.parse(playListStr);
-      } catch(e) {
-        playList = [];
-      }
-      return playList;
+    exec(`mplayer ${url}`, '', () => {
+      resolve();
     });
-    return list;
-  }
-
-  let weatheList = await getWeatherInfo();
-
-  if (weatheList.length === 0) {
-    weatheList = getWeatherInfo(); // 再试一次
-  }
-  await browser.close();
-
-  if (weatheList[0] && weatheList[0].src) {
-    console.log(weatheList[0].src)
-    await playAudio(weatheList[0].src);
-  }
+  });
 }
 
-module.exports = getWeather;
+const getUrl = function getXmlyMediaUrl() {
+  return new Promise((resolve, rejects) => {
+    request.get(xmlyAPI, (error, response, body) => {
+      try {
+        const res = JSON.parse(body);
+        if (!error && body && res.audioUrl) {
+          const url = res.audioUrl;
+          resolve(url);
+        } else {
+          rejects('喜马拉雅数据错误');
+        }
+      } catch(e) {
+        rejects(e);
+      }
+    });
+  });
+}
+
+module.exports = function  wether () {
+  getUrl().then(url => {
+    playAudio(url);
+  }).catch(e => {
+    // wether();
+  }) ;
+}
